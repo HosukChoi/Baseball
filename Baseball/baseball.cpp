d42f1a2b749423baf6fd7d495604eb68efb568e2
darkchsq <darkchsq@naver.com>
2026-05-28 13:07:02 +0900

    [feature] page 19 - Add guess function in Baseball class

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -1,4 +1,10 @@
+#include <string>
+#include <stdexcept>
+using std::string;
+
 class Baseball {
 public:
-
+	void guess(const string& string) {
+		throw std::length_error("Must be three letters.");
+	}
 };
--- Baseball/baseball_test.cpp
+++ Baseball/baseball_test.cpp
@@ -5,6 +5,11 @@ TEST(BaseballGame, TryGameTest) {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), std::length_error);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
