96d5334159ff4f91c91b05d93d97c346c583d4dc
darkchsq <darkchsq@naver.com>
2026-05-28 13:15:12 +0900

    [refactor] page 29 - Update Test code   - Delete duplicate code

--- Baseball/baseball_test.cpp
+++ Baseball/baseball_test.cpp
@@ -1,18 +1,23 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
-}
-
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+class BaseballFixture : public testing::Test {
+public: 
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), std::length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (std::exception e) {
+			//PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWHenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), std::invalid_argument);
+TEST_F(BaseballFixture, ThrowExceptionWHenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main() {
